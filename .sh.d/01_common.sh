756bdb52d4e864704b77e3bec9a335c39058c22e
Mesar Hameed <mesar.hameed@gmail.com>
2014-04-01 21:33:13 +0200

    pull out repetative code into common functions.

--- .sh.d/01_common.sh
+++ .sh.d/01_common.sh
@@ -3,7 +3,7 @@ DEBUG=1
 
 logMsg() {
     if [ $DEBUG -ne 0 ]; then
-        echo `date '+%Y/%m/%d %H:%M:%S'` "$1"
+        echo 1>&2 `date '+%Y/%m/%d %H:%M:%S'` "$@"
     fi
 }
 
@@ -12,3 +12,16 @@ isAddon() {
 	test -d ${MR_REPO}/addon -a -f sconstruct -a -f buildVars.py
 }
 
+isAddonOrDie() {
+	if ! $(isAddon); then
+		logMsg "Warning: Doesn't seem to be an addon, aborting."
+		exit 1
+	fi
+}
+
+hasStableBranchOrDie() {
+    if git branch -r | grep -qv "origin/stable"; then
+        logMsg "Warning: this addon has no stable branch, aborting."
+        exit 1
+    fi
+}
