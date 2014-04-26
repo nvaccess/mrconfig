3756a63112361b4a5983c84643ac2e5cf2ef095a
Mesar Hameed <mesar.hameed@gmail.com>
2014-04-26 17:31:47 +0200

    01_nvda2svn.sh:findRevs, make sure there is a userGuide-stats.txt for each revision that we provide for translation.

--- .sh.d/01_nvda2svn.sh
+++ .sh.d/01_nvda2svn.sh
@@ -21,6 +21,12 @@ findRevs() {
     cd scripts
     ./findRevs.py --langs $langs
     cd ..
+    # Calculate userGuide-stats.txt for every revision.
+    find */userGuide-diffs/ -maxdepth 1 -mindepth 1 -type d | grep -vi ".svn"  || while read rev; do
+        pushd $rev >/dev/null 2>&1
+        python ../../../scripts/stats.py
+        popd >/dev/null 2>&1
+    done
     svn -q add  */settings */userGuide-diffs/* */changes-diffs/* */symbols-diffs/*
     svn commit -m "All langs: new revisions for translation." */settings */userGuide-diffs/* */changes-diffs/* */symbols-diffs/*
 }
