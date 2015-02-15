21a12bfb739371cb86df9e92f106c255ea6251ae
Mesar Hameed <mesar.hameed@gmail.com>
2015-02-15 08:24:49 +0000

    01_svn2nvda.sh: use and update the lastcommittedRevision settings key.

--- .sh.d/01_svn2nvda.sh
+++ .sh.d/01_svn2nvda.sh
@@ -22,16 +22,11 @@ svn2nvda () {
     ls -1 */settings | while read file; do
         lang=$(dirname $file)
         logMsg "Processing $lang" 
-        interface_last_date=$(python scripts/db.py -f $file -g nvda.interface.lastSubmittedDate)
-        if test "0" = "${interface_last_date}" ; then
-            # at the moment that key is never there, should we introduce this key
-            # or is it ok to always just check for commits in the last month.
-            # This should be safe, since we commit to nvda git 2 times a month, so
-            # there shouldn't be overlooked commits.
-            interface_last_date=$(date -u --date='now - 1 month' +'%F %T %z')
-            #date -u +'%F %T %z'
+        lastSubmittedSvnRev=$(python scripts/db.py -f $file -g nvda.lastSubmittedSvnRev)
+        if test "0" = "${lastSubmittedSvnRev}"; then
+            lastSubmittedSvnRev=1
         fi
-        needsCommitting=$(svn log -r"{$interface_last_date}":head ${lang}/nvda.po | grep -iP "r[0-9]+ \|" | grep -viP "commitbot|mhameed" | wc -l)
+        needsCommitting=$(svn log -r${lastSubmittedSvnRev}:head ${lang}/nvda.po | grep -iP "r[0-9]+ \|" | grep -viP "commitbot" | wc -l)
         if test "$needsCommitting" != "0" && python -m poChecker $lang/nvda.po ; then
             _cp $lang/nvda.po source/locale/$lang/LC_MESSAGES/nvda.po
         fi
@@ -47,9 +42,11 @@ svn2nvda () {
             stats=$(git -C "$gitDir" diff --cached --numstat --shortstat)
             echo "L10n updates for: $lang\nFrom translation svn revision: $svnRev\n\nAuthors:\n$authors\n\nStats:\n$stats" | 
             git -C "$gitDir" commit -F -
+            python scripts/db.py -f $file -s nvda.lastSubmittedSvnRev "$svnRev"
         fi
     done
     git -C "$gitDir" checkout master
     git -C "$gitDir" stash pop || true
     echo "All languages processed, use stg to edit authors/provide additional information., also don't forget to push to try repo to make sure a snapshot can be built."
+    echo "When all done don't forget to commit the updated metadata in srt/*/settings"
 }
