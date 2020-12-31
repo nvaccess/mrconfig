52407c4b2967aff1fc23d00ee61426e864cd836d
Reef Turner <reef@nvaccess.org>
2020-12-31 10:12:29 +0800

    common issue: mising po file

--- .sh.d/01_addon2svn.sh
+++ .sh.d/01_addon2svn.sh
@@ -13,6 +13,18 @@ addon2settings() {
     done
 }
 
+_addFreshPoFile() {
+    addonName=$1
+    lang=$2
+    addonPath=$lang/add-ons/${addonName}
+    addonPoPath=${addonPath}/nvda.po
+    cp /tmp/${addonName}.pot ${addonPoPath}
+    sed -i -e "s/Language: /Language: $lang/g" ${addonPoPath}
+    msgmerge --no-location -U ${addonPoPath} /tmp/${addonName}-merge.pot
+    svn add ${addonPoPath}
+    svn commit -m "${lang}: ${addonName} ready to be translated."  ${addonPath}
+}
+
 # Run by cron with path ${PathToMrRepo}/addons/<addon Name>/
 addon2svn() {
     isAddonOrDie
@@ -44,36 +56,37 @@ addon2svn() {
         if [ "$want" != "1" ]; then
             continue
         fi
-        if [ ! -d $lang/add-ons/${addonName} ]; then
+        addonPath=$lang/add-ons/${addonName}
+        addonPoPath=${addonPath}/nvda.po
+        if [ ! -d ${addonPath} ]; then
             logMsg "Wanted, but not already in svn, providing it."
-            svn mkdir --parents $lang/add-ons/${addonName}
-            cp /tmp/${addonName}.pot $lang/add-ons/${addonName}/nvda.po
-            sed -i -e "s/Language: /Language: $lang/g" $lang/add-ons/${addonName}/nvda.po
-            msgmerge --no-location -U $lang/add-ons/${addonName}/nvda.po /tmp/${addonName}-merge.pot
-            svn add $lang/add-ons/${addonName}/nvda.po
-            svn commit -m "${lang}: ${addonName} ready to be translated."  $lang/add-ons/${addonName}/
+            svn mkdir --parents ${addonPath}
+            _addFreshPoFile ${addonName} ${lang}
+        elif [ ! -f ${addonPoPath} ]; then
+            logMsg "Wanted, but po file is missing, restoring file."
+            _addFreshPoFile ${addonName} ${lang}
         else
             logMsg "Already available for translation, merging in new messages."
 
             # Statistics before merging pot.
-            bfuzzy=$(pocount $lang/add-ons/${addonName}/nvda.po | grep -i fuzzy | awk '{print $2}')
-            buntranslated=$(pocount $lang/add-ons/${addonName}/nvda.po | grep -i untranslated | awk '{print $2}')
+            bfuzzy=$(pocount ${addonPoPath} | grep -i fuzzy | awk '{print $2}')
+            buntranslated=$(pocount ${addonPoPath} | grep -i untranslated | awk '{print $2}')
             bmsg="$bfuzzy fuzzy and $buntranslated untranslated"
 
-            msgmerge --no-location -U $lang/add-ons/${addonName}/nvda.po /tmp/${addonName}-merge.pot
+            msgmerge --no-location -U ${addonPoPath} /tmp/${addonName}-merge.pot
 
             # Statistics after merging pot.
-            afuzzy=$(pocount $lang/add-ons/${addonName}/nvda.po | grep -i fuzzy | awk '{print $2}')
-            auntranslated=$(pocount $lang/add-ons/${addonName}/nvda.po | grep -i untranslated | awk '{print $2}')
+            afuzzy=$(pocount ${addonPoPath} | grep -i fuzzy | awk '{print $2}')
+            auntranslated=$(pocount ${addonPoPath} | grep -i untranslated | awk '{print $2}')
             amsg="$afuzzy fuzzy and $auntranslated untranslated"
 
             if [ "$bmsg" != "$amsg" ]; then
-                # need to commit, because before and after are diffrent.
-                svn commit -m "${lang}: ${addonName} merged in ${amsg} messages"  $lang/add-ons/${addonName}/nvda.po
+                # need to commit, because before and after are different.
+                svn commit -m "${lang}: ${addonName} merged in ${amsg} messages"  ${addonPoPath}
             else
                 # nothing has changed, dont need to action.
                 # revert because comments/timestamps in po file might have changed.
-                svn revert $lang/add-ons/${addonName}/nvda.po
+                svn revert ${addonPoPath}
             fi
         fi
     done
--- docs/commonIssues.md
+++ docs/commonIssues.md
@@ -12,3 +12,18 @@ Docs: https://github.com/nvdaaddons/nvdaaddons.github.io/wiki/MakeAddonsTranslat
 
 Corresponding check for branch: `hasStableBranchOrDie` in `01_common.sh`
 
+
+### Missing po file:
+
+Cron email with message:
+```
+<lang ID> wants <addon name>: 1
+Already available for translation, merging in new messages.
+translate.tools.pocount: ERROR: cannot process <lang ID>/add-ons/<addon name>/nvda.po: does not exist
+translate.tools.pocount: ERROR: cannot process <lang ID>/add-ons/<addon name>/nvda.po: does not exist
+msgmerge: error while opening "<lang ID>/add-ons/<addon name>/nvda.po" for reading: No such file or directory
+```
+
+The po file is missing. Perhaps it was deleted accidentally or the translator decided they would not translate for the add-on.
+
+Scripts are being updated to handle this case. The file will be re-added. If the translator no longer wants to work on this add-on, the settings file should be updated to reflect that instead. 
