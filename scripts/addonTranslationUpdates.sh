b0d73a91981d5403be19962bbc4ee111a6b89577
Reef Turner <reef@nvaccess.org>
2021-01-11 19:24:04 +0800

    Save logs for translations updates
    The logs are useful to inspect if there is strange behaviour
    but none of the commands failed.

---
+++ scripts/addonTranslationUpdates.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+AddonName="${1}"
+PathToAddons=/home/nvdal10n/mr/addons
+LogDir=/home/nvdal10n/translationUpdateLogs/
+
+_doAddonTranslationUpdate(){
+  set -x # for echo of commands and variables
+  addonPath=$1
+
+  if cd "${addonPath}"; then
+    echo Exit early
+    set +x
+    return 1
+  fi
+  if mr addon2svn; then
+    echo Exit early
+    set +x
+    return 1
+  fi
+  if mr svn2addon; then
+    echo Exit early
+    set +x
+    return 1
+  fi
+}
+
+logPath="${LogDir}/addon-${AddonName}.log"
+if _doAddonTranslationUpdate "${PathToAddons}/${AddonName}" &>"${logPath}"; then
+  cat "${logPath}";
+fi
