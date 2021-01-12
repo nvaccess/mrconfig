7a3c3b63851bd0d36bbde5cc60a0f91f9da1597e
Reef Turner <reef@nvaccess.org>
2021-01-12 18:04:44 +0800

    bash variables are global
    Don't override the logPath var

--- scripts/nvdaTranslationUpdates.sh
+++ scripts/nvdaTranslationUpdates.sh
@@ -5,29 +5,29 @@ LogDir=/home/nvdal10n/translationUpdateLogs
 _doNVDATranslationUpdate(){
   error=0
   set -x # for echo of commands and variables
-  logPath="${LogDir}/mr-up.log"
-  cd $PathToMrRepo && mr up &> $logPath;
+  upLogPath="${LogDir}/mr-up.log"
+  cd $PathToMrRepo && mr up &> $upLogPath;
   if [ $? -ne 0 ] ; then
     echo mr up failed
     error=1
   fi
 
-  logPath="${LogDir}/mr-svn2nvda.log"
-  cd ${PathToMrRepo}/srt/ && mr svn2nvda &> $logPath
+  svn2nvdaLogPath="${LogDir}/mr-svn2nvda.log"
+  cd ${PathToMrRepo}/srt/ && mr svn2nvda &> $svn2nvdaLogPath
   if [ $? -ne 0 ] ; then
     echo mr svn2nvda failed
     error=1
   fi
 
-  logPath="${LogDir}/mr-mergePot.log"
-  cd ${PathToMrRepo}/srt/ && mr mergePot &> $logPath
+  mergePotLogPath="${LogDir}/mr-mergePot.log"
+  cd ${PathToMrRepo}/srt/ && mr mergePot &> $mergePotLogPath
   if [ $? -ne 0 ] ; then
     echo mr mergePot failed
     error=1
   fi
 
-  logPath="${LogDir}/mr-findRevs.log"
-  cd ${PathToMrRepo}/srt/ && mr findRevs &> $logPath
+  findRevsLogPath="${LogDir}/mr-findRevs.log"
+  cd ${PathToMrRepo}/srt/ && mr findRevs &> $findRevsLogPath
   if [ $? -ne 0 ] ; then
     echo mr findRevs failed
     error=1
