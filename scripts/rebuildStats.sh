56579ed37d363504f7b863680bc46bec0ccc7694
Cyrille Bougot <cyrille.bougot2@laposte.net>
2024-05-28 10:58:07 +0200

    Updates for the structure diff scripts: - now also checks the diffs of the change log - indicate the files being compared in the report - fixed a bug where no diff could be detected for files of unequal length

--- scripts/rebuildStats.sh
+++ scripts/rebuildStats.sh
@@ -2,13 +2,19 @@
 
 # Called by convertOne.sh, with current directory set to SRT_PATH/<lang>
 
-nextRev=`ls -1 userGuide-newRevisions/ | head -n 1`
-if [ "$nextRev" == "" ]; then
-    echo "No revision found, unable to calculate structure difference."
-    exit
-fi
-
 scriptsDir="../../scripts"
 lang=$(basename `pwd`)
 
+nextRev=`ls -1 changes-newRevisions/ | head -n 1`
+if [ "$nextRev" == "" ]; then
+    echo "No revision found, unable to calculate changes structure difference."
+    exit
+fi
+python3 ${scriptsDir}/structDiffMd.py $lang changes-newRevisions/$nextRev/changes.md changes.md changes-structureDifferences.txt
+
+nextRev=`ls -1 userGuide-newRevisions/ | head -n 1`
+if [ "$nextRev" == "" ]; then
+    echo "No revision found, unable to calculate userGuide structure difference."
+    exit
+fi
 python3 ${scriptsDir}/structDiffMd.py $lang userGuide-newRevisions/$nextRev/userGuide.md userGuide.md userGuide-structureDifferences.txt
